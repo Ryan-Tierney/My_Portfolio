88aa53304b3cde6ae9546fe6c65530ca65d2ff7f
Ryan-Tierney <Ryan.Tierney92@gmail.com>
2021-07-31 18:23:13 -0500

    adding pages to heroku

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 <?php include_once("index.html"); ?>
+<?php include_once("portfolio_pages/Finish_line.html"); ?>
+<?php include_once("portfolio_pages/GamingBacklog.html"); ?>
+<?php include_once("portfolio_pages/SWLShop.html"); ?>
+<?php include_once("portfolio_pages/Create&Express.html"); ?>
